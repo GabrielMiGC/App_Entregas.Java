262d20564f673d6c45871b5334ae5936a6106614
Aline Vitória <78926056+alinevitoriaw@users.noreply.github.com>
2023-06-26 09:15:17 -0300

    Update Pagamento.java

--- Pagamento.java
+++ Pagamento.java
@@ -32,4 +32,75 @@ public class Pagamento {
 		DataVencimento = dataVencimento;
 	}
 	
-}
+	public void realizarPagamento() {
+	        Scanner scanner = new Scanner(System.in);
+	        
+	        System.out.println("Escolha a forma de pagamento:");
+	        System.out.println("1. Cartão de crédito");
+	        System.out.println("2. Cartão de débito");
+	        System.out.println("3. Pix");
+	        
+	        int opcao = scanner.nextInt();
+	        switch (opcao) {
+	            case 1:
+	                System.out.println("Pagamento com cartão de crédito selecionado.");
+	                processarPagamentoCartaoCredito();
+	                break;
+	                
+	            case 2:
+	                System.out.println("Pagamento com cartão de débito selecionado.");
+	                processarPagamentoCartaoDebito();
+	                break;
+	            
+	            case 3:
+	            	System.out.println("Pagamento com Pix seleciondo.");
+	            	processarPagamentoPix();
+	            	break;
+	                
+	            default:
+	                System.out.println("Opção inválida. Tente novamente.");
+	                break;
+	        }
+	        scanner.close();
+	    }
+	    
+	    private void processarPagamentoCartaoCredito() {
+	        Scanner scanner = new Scanner(System.in);
+	        
+	        System.out.println("Informe o valor do pagamento:");
+	        double valor = scanner.nextDouble();
+	        
+	        System.out.println("Informe a quantidade de parcelas:");
+	        int parcelas = scanner.nextInt();
+	        
+	        System.out.println("Processando pagamento com cartão de crédito...");
+	        System.out.println("Valor: " + valor);
+	        System.out.println("Parcelas: " + parcelas);
+	        
+	        scanner.close();
+	    }
+	    
+	    private void processarPagamentoCartaoDebito() {
+	        Scanner scanner = new Scanner(System.in);
+	        
+	        System.out.println("Informe o valor do pagamento:");
+	        double valor = scanner.nextDouble();
+	        
+	        System.out.println("Processando pagamento com cartão de débito...");
+	        System.out.println("Valor: " + valor);
+	        
+	        scanner.close();
+	    }
+	    
+	    private void processarPagamentoPix() {
+	        Scanner scanner = new Scanner(System.in);
+	        
+	        System.out.println("Informe o valor do pagamento:");
+	        double valor = scanner.nextDouble();
+	        
+	        System.out.println("Processando pagamento com Pix..");
+	        System.out.println("Valor: " + valor);
+	        
+	        scanner.close();
+	    }
+	}
