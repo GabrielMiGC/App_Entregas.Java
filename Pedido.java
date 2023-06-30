45e7f2f6b23291246a4b45b476bfbfa8c47d6b69
Aline Vitória <78926056+alinevitoriaw@users.noreply.github.com>
2023-06-30 15:29:23 -0300

    Update Pedido.java

--- Pedido.java
+++ Pedido.java
@@ -1,23 +1,83 @@
 package AppEntregas;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Pedido {
-    Restaurante restaurante;
-    Prato prato;
-    Endereco endereco;
+    private String restaurante;
+    private Prato prato;
+    private Endereco endereco;
+    private boolean cancelado;
 
-    public String StatusDoPedido() {
-        if (restaurante != null && prato != null && endereco != null) {
-            // concluído!!
-            return "Pedido de " + prato.nomePrato + " concluído";
-        } else if (restaurante != null && prato != null) {
-            // na lista para entrega
-            return "Pedido de " + prato.nomePrato + " a caminho";
-        } else if (restaurante != null) {
-            // em produção
-            return "Pedido está sendo feito";
-        } else {
-            // quando algum atributo está nulo
-            return "Pedido inválido";
+    public Pedido() {
+        this.restaurante = null;
+        this.prato = null;
+        this.endereco = null;
+        this.cancelado = false;
+    }
+
+    public void exibirMenu() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("***** Menu *****");
+            System.out.println("1. Fazer Pedido");
+            System.out.println("2. Cancelar Pedido");
+            System.out.println("0. Sair");
+
+            System.out.print("Escolha uma opção: ");
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    fazerPedido();
+                    break;
+                case 2:
+                    cancelarPedido();
+                    break;
+                case 0:
+                    return;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
         }
     }
+    public void fazerPedido(String nomeRestaurante) {
+        restaurante = nomeRestaurante;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("***** Faça seu pedido e adicione seu endereço *****");
+        System.out.print("Prato: ");
+        String nomePrato = scanner.nextLine();
+        prato = new Prato(nomePrato);
+
+        System.out.print("Endereço: ");
+        String enderecoCompleto = scanner.nextLine();
+        endereco = new Endereco(enderecoCompleto);
+    }
+
+    //public void cancelarPedidos(){}
+    
+    public void arqPedidos() {
+        String nomeArquivo = "Pedidos feitos.txt";
+        
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo, true))) {
+            writer.write("Restaurante: " + restaurante);
+            writer.newLine();
+            writer.write("Prato: " + prato.getNomePrato());
+            writer.newLine();
+            writer.write("Endereço: " + endereco.getEnderecoCompleto());
+            writer.newLine();
+            writer.write("Status: " + getStatusDoPedido());
+            writer.newLine();
+            System.out.println("Pedido salvo com sucesso no arquivo: " + nomeArquivo);
+        } catch (IOException e) {
+            System.out.println("Erro ao escrever o pedido no arquivo: " + e.getMessage());
+        }
+    }
+    //public void removerPedidoCancelado(){}
 }
+    //status do pedido
